050b70486541b4e5289c78c9a024623681638ef2
Julia <jsilva@middlebury.edu>
2019-05-06 23:45:31 -0400

    playing different songs

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,19 +2,10 @@ import React, {Component} from 'react';
 import { compose } from 'recompose';
 import '../App/App.css';
 import { Button, Container, Row } from 'reactstrap';
-//import MusicPlayer from '../MusicPlayer/index.js'
-//import MusicPlayer2 from '../MusicPlayer/index.js'
+import MusicPlayer from '../MusicPlayer/index.js'
 
 import { withAuthorization, withEmailVerification } from '../Session';
 import Messages from '../Messages';
-//import Songs from '../Songs';
-
-function changeSong(song) {
-  document.getElementById("music-player").pause();
-  document.getElementById("music-player").setAttribute('src', song);
-  document.getElementById("music-player").load();
-  document.getElementById("music-player").play();
-}
 
 class HomePage extends Component {
   constructor(props) {
@@ -65,17 +56,8 @@ class HomePage extends Component {
 
         <Container id="intro">
           <h1>Your Music Player</h1>
-          <Row>
-              <audio id="music-player" controls>
-                <source src="https://firebasestorage.googleapis.com/v0/b/reverb-9081f.appspot.com/o/rizkyrilos%20-%20Queen%20-%20Bohemian%20Rhapsody.mp3?alt=media&token=56ed2628-668d-4546-a657-692f4d51899e"/>
-              </audio>
-              <Button color="warning">Stream Now</Button>
-          </Row>
-          <br />
-          <br />
-          <Button onClick="changeSong('https://firebasestorage.googleapis.com/v0/b/reverb-9081f.appspot.com/o/rizkyrilos%20-%20Queen%20-%20Bohemian%20Rhapsody.mp3?alt=media&token=56ed2628-668d-4546-a657-692f4d51899e')">Bohemian Rhapsody</Button>
-          <div />
-          <Button onClick="changeSong('https://firebasestorage.googleapis.com/v0/b/reverb-9081f.appspot.com/o/Skrillex%20%26%20Poo%20Bear%20-%20Would%20You%20Ever.mp3?alt=media&token=1de62308-3d33-44cc-95aa-443ff5953d2a')">Would You Ever</Button>
+          <MusicPlayer />
+          <Messages />
         </Container>
       )
     }
--- src/components/MusicPlayer/index.js
+++ src/components/MusicPlayer/index.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Button, Container, Row } from 'reactstrap';
 
+function changeSong(song) {
+  document.getElementById("music-player").pause();
+  document.getElementById("music-player").setAttribute('src', song);
+  document.getElementById("music-player").load();
+  document.getElementById("music-player").play();
+}
 
 class MusicPlayer extends Component {
     constructor(props) {
@@ -24,9 +31,23 @@ class MusicPlayer extends Component {
     console.log("The prop is", this.props.currentSong);
     console.log("Render called");
     return (
-        <audio id="music-player" controls ref="audio">
-          <source src={this.props.currentSong} />
-        </audio>
+      <Container>
+      <Row>
+          <audio id="music-player" controls>
+            <source src="https://firebasestorage.googleapis.com/v0/b/reverb-9081f.appspot.com/o/rizkyrilos%20-%20Queen%20-%20Bohemian%20Rhapsody.mp3?alt=media&token=56ed2628-668d-4546-a657-692f4d51899e"/>
+          </audio>
+          <Button color="warning">Stream Now</Button>
+      </Row>
+      <br />
+      <br />
+      <Button onClick={() => {
+        changeSong('https://firebasestorage.googleapis.com/v0/b/reverb-9081f.appspot.com/o/rizkyrilos%20-%20Queen%20-%20Bohemian%20Rhapsody.mp3?alt=media&token=56ed2628-668d-4546-a657-692f4d51899e')
+      }}>Bohemian Rhapsody</Button>
+      <div />
+      <Button onClick={()=> {
+        changeSong('https://firebasestorage.googleapis.com/v0/b/reverb-9081f.appspot.com/o/Skrillex%20%26%20Poo%20Bear%20-%20Would%20You%20Ever.mp3?alt=media&token=1de62308-3d33-44cc-95aa-443ff5953d2a')
+      }}>Would You Ever</Button>
+      </Container>
       );
   }
 }
